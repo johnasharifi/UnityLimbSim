5ce1338e18a13d4dec7f0c05a2a8d97cc25d5589
jsharifi <johnasharifi@gmail.com>
2018-12-17 18:19:08 -0800

    committing first upload

---
+++ ScriptNode.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An individual node in a limb simulation.
+/// angular_max: a value in range [0-1] which represents flexibility of this node
+/// ref_capsule: a local gameObject which represents the actual limb
+/// </summary>
+public class ScriptNode : MonoBehaviour {
+	[Range(0f, 1f)]
+	public float angular_max;
+
+	[SerializeField]
+	private GameObject ref_capsule;
+	private Quaternion natural_local;
+
+	// For debugging. Delete to speed up
+	[SerializeField]
+	private LineRenderer lr_assemblage;
+	[SerializeField]
+	private LineRenderer lr_ideal;
+	public bool indicator;
+
+	// Use this for initialization
+	void Start () {
+		ReaffixNaturalLocal ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	/// <summary>
+	/// Stores new local position and treats it as the default rotation off of parent.
+	/// </summary>
+	[ContextMenu("Reaffix the default rotation of this node to current rotation of this node")]
+	public void ReaffixNaturalLocal() {
+		natural_local = transform.localRotation;
+	}
+
+	/// <summary>
+	/// Computes vector from self's terminus, to target
+	/// </summary>
+	/// <param name="ee">Ee. An end-effector. A terminal node in a sequence of limbs</param>
+	/// <param name="target">Target. An object in space</param>
+	public void CorrectError(ref ScriptNode ee, ref GameObject target) {
+		Vector3 error = target.transform.position - ee.GetTerminus ();
+		ApplyDeltaLook (ref ee, error);
+	}
+
+	/// <summary>
+	/// Applies a rotation which maps vector space from the vector "limb-to-end-effector" to "limb-to-end-effector-plus-error"
+	/// </summary>
+	/// <param name="ee">Ee. An end-effector. A terminal node in a sequence of limbs</param>
+	/// <param name="error">Error. The difference between end-effector's current position, and end-effector's ideal position</param>
+	public void ApplyDeltaLook(ref ScriptNode ee, Vector3 error) {
+		Vector3 la = GetLocalAssemblage (ref ee);
+		Quaternion root_rot = transform.root.rotation;
+		float dot_modifier = (Mathf.Abs (Vector3.Dot (Vector3.forward, root_rot * Vector3.forward)) >= 0.5f ? 1 : -1);
+		root_rot = Quaternion.Euler (root_rot.eulerAngles.x, root_rot.eulerAngles.y * dot_modifier, root_rot.eulerAngles.z);
+
+		Quaternion delta = Quaternion.FromToRotation (root_rot * la, root_rot * (la + error));
+
+		Quaternion ideal_local = Quaternion.Inverse (transform.parent.rotation) * (transform.rotation * delta);
+		Quaternion limited_local = Quaternion.RotateTowards (natural_local, ideal_local, angular_max * 360f);
+
+		// max rotation at one update: 36 degrees
+		Quaternion incremental_local = Quaternion.RotateTowards(transform.localRotation, limited_local, 0.1f * 360f);
+		transform.localRotation = incremental_local;
+
+		lr_assemblage.gameObject.SetActive (indicator);
+		lr_ideal.gameObject.SetActive (indicator);
+		lr_assemblage.SetPositions (new Vector3[] { transform.position, transform.position + la });
+		lr_ideal.SetPositions (new Vector3[] { transform.position, transform.position + la + error });
+	}
+
+	/// <summary>
+	/// Gets the vector from self to end-effector
+	/// </summary>
+	/// <returns>local assemblage.</returns>
+	/// <param name="ee">Ee. An end-effector</param>
+	private Vector3 GetLocalAssemblage(ref ScriptNode ee) {
+		return(ee.GetTerminus() - transform.position);
+	}
+
+	/// <summary>
+	/// Gets the representation of the terminus of this limb in world coordinates.
+	/// </summary>
+	/// <returns>Position of the end of this limb, in world coordinates.</returns>
+	public Vector3 GetTerminus() {
+		return(transform.position + transform.forward * (ref_capsule.transform.localPosition.z + ref_capsule.transform.localScale.y));
+	}
+}
---
+++ ScriptRoot.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptRoot : MonoBehaviour {
+	public enum display_modes: int {instant, sequence, target0, modulate_a_b};
+	public display_modes display_mode;
+
+	private ScriptNode[] nodes;
+
+	public List<LimbTrainer> trainers = new List<LimbTrainer>();
+
+	private Vector3 last_instant_position;
+	public float ab_alpha = 0f;
+
+	// Use this for initialization
+	void Start () {
+		last_instant_position = transform.position;
+		nodes = transform.GetComponentsInChildren<ScriptNode> ();
+
+		foreach (LimbTrainer t in trainers)
+			t.path = nodes;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (display_mode == display_modes.instant) {
+			DisplayInstant ();
+		}
+
+		if (display_mode == display_modes.sequence) {
+			DisplaySequence ();
+		}
+		if (display_mode == display_modes.target0) {
+			DisplayTarget ();
+		}
+		if (display_mode == display_modes.modulate_a_b && trainers.Count > 1) {
+			DisplayModulateAB ();
+		}
+	}
+
+	private void DisplayModulateAB() {
+		int f1 = 0;
+		int f2 = 1;
+
+		Vector3 delta = transform.position - last_instant_position;
+		last_instant_position = transform.position;
+		Vector3 movement_direction = (trainers [f1].target.transform.position - trainers [f2].target.transform.position);
+		float d = Mathf.Max(1f, Vector3.Distance (trainers [f1].target.transform.position, trainers [f2].target.transform.position));
+		ab_alpha = (ab_alpha + Vector3.Project (delta, movement_direction).magnitude / d) % 2f;
+
+		float alpha = Mathf.Abs (ab_alpha - 1f);
+
+		List<Quaternion> frame_1 = trainers [f1].GetSolution ();
+		List<Quaternion> frame_2 = trainers [f2].GetSolution ();
+
+		for (int i = 0; i < trainers[f1].path.Length; i++) {
+			trainers [f1].path [i].transform.rotation = Quaternion.Lerp (frame_1 [i], frame_2 [i], alpha);
+		}
+	}
+
+	/// <summary>
+	/// Displays limb orientations for only first target
+	/// </summary>
+	private void DisplayTarget() {
+		float theta = 0f;
+		int f1 = Mathf.FloorToInt (theta);
+
+		List<Quaternion> frame = trainers [f1].GetSolution ();
+		for (int i = 0; i < trainers[f1].path.Length; i++) {
+			trainers [f1].path [i].transform.rotation = frame [i];
+		}
+	}
+
+	/// <summary>
+	/// Displays limb orientations at an interpolation between instants
+	/// </summary>
+	private void DisplayInstant() {
+		float theta = Time.timeSinceLevelLoad % (1f * trainers.Count);
+		int f1 = Mathf.FloorToInt (theta);
+
+		List<Quaternion> frame = trainers [f1].GetSolution ();
+		for (int i = 0; i < trainers[f1].path.Length; i++) {
+			trainers [f1].path [i].transform.rotation = frame [i];
+		}
+	}
+
+	/// <summary>
+	/// Forces limbs to orient in the way which the limb simulator has concluded is best
+	/// </summary>
+	private void DisplaySequence() {
+		List<List<Quaternion>> dispositions = new List<List<Quaternion>> ();
+		for (int i = 0; i < trainers.Count; i++) {
+			dispositions.Add(trainers [i].GetSolution ());
+		}
+
+		float theta = Time.timeSinceLevelLoad % (1f * trainers.Count);
+		float alpha = Time.timeSinceLevelLoad % 1f;
+
+		int f1 = Mathf.FloorToInt (theta);
+		int f2 = (f1 + 1) % trainers.Count;
+
+		List<Quaternion> frame_1 = dispositions [f1];
+		List<Quaternion> frame_2 = dispositions [f2];
+
+		for (int i = 0; i < trainers[f1].path.Length; i++) {
+			trainers [f1].path [i].transform.rotation = Quaternion.Lerp (frame_1 [i], frame_2 [i], alpha);
+		}
+	}
+}
+	
+/// <summary>
+/// A parameterizable manager for specifying a target, iterating on solutions, and returning a solution.
+/// trainer_node: a node in a series of limbs which we wish to move toward a target
+/// target: an object in space which we will move a limb toward
+/// </summary>
+[System.Serializable]
+public class LimbTrainer {
+	private List<Quaternion> dispositions;
+	// likely overkill, reduce iter_count as desired
+	private const int iter_count = 50;
+
+	// populated by the ScriptRoot
+	public  ScriptNode[] path;
+
+	public ScriptNode trainer_node;
+	public GameObject target;
+
+	private List<Quaternion> solution;
+	private Vector3 last_inst;
+
+	/// <summary>
+	/// Finds a list of quaternions which produces a limb orientation which brings the terminus of a trainer node toward the target
+	/// </summary>
+	/// <returns>The solution.</returns>
+	public List<Quaternion> GetSolution() {
+		if (solution != null && last_inst == target.transform.position)
+			return(solution);
+		
+		solution = new List<Quaternion> ();
+
+		for (int iters = 0; iters < iter_count; iters++) {
+			foreach (ScriptNode sn in path) {
+				sn.CorrectError (ref trainer_node, ref target);
+			}
+		}
+
+		foreach (ScriptNode sn in path)
+			solution.Add (sn.transform.rotation);
+
+		last_inst = target.transform.position;
+
+		return(solution);
+	}
+}
